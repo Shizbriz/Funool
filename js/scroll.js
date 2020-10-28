918bf9aeab5ad6a9d48777c0ece0825d286a4626
Shizbriz <56767854+Shizbriz@users.noreply.github.com>
2020-10-28 05:28:45 +0100

    first commit

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+let toog = function(idh,ids) {
+    let e = document.getElementById(idh);
+    let f = document.getElementById(ids)
+       e.style.display = 'none';
+       f.style.display = 'grid'
+    }
+
+let side = function(id){
+   let e= document.getElementById(id);
+
+   if(e.style.display === 'block'){e.style.display ='none';}
+else{e.style.display ='block'}
+}
+   
